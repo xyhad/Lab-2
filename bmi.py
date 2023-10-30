94254eac6a071ebf1dfabaf2aaa889471e4860f4
MuhammadIrsyhad <muhammadirsyhad.22@ichat.sp.edu.sg>
2023-10-30 14:49:27 +0800

    Added return values for each weight classification

--- bmi.py
+++ bmi.py
@@ -5,10 +5,16 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
     if bmi < 18.5:
         print("Weight classification - Under Weight")
+        return_val = -1
+        return return_val
     elif bmi<=25.0 and bmi>=18.5:
         print("Weight classification - Normal weight")
+        return_val = 0
+        return return_val
     elif bmi > 25.0:
         print("Weight classification - Over Weight")
+        return_val = 1
+        return return_val
 
 
 calculate_bmi(weight=57, height=1.73)
